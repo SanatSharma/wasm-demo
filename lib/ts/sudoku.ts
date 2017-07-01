781459fa46aced7f6f4f9ae0fe30aaae6aa54444
Sanat Sharma <sanatcolumban@gmail.com>
2017-06-30 18:33:14 -0700

    Added correct implementation for 9*9 board

--- lib/ts/sudoku.ts
+++ lib/ts/sudoku.ts
@@ -1,4 +1,4 @@
-let model: number[][];
+let model = new Array(9);
 let modelLength: number = 9;
 
 main();
@@ -7,21 +7,26 @@ function initBoard() {
     let i: number = 0;
     let j: number = 0;
 
+    console.log(model.length);
     for (i = 0; i < modelLength; i++){
+        model[i] = new Array(9);
         for (j = 0; j < modelLength; j++){
             model[i][j] = 0;
         }
     }
 
-    // Create the initial situation
+    
+    // Create the initial board
 	model[0][0] = 9 ;
 	model[0][3] = 2 ;
 	//model[0][6] = 7 ;
 	model[0][7] = 5 ;
 
-    /*model[1][0] = 6 ;
+    
+    model[1][0] = 6 ;
     model[1][4] = 5 ;
-    model[1][7] = 4 ;*/
+    model[1][7] = 4 ;
+    
 
     model[2][1] = 2 ;
     model[2][3] = 4 ;
@@ -55,7 +60,6 @@ function initBoard() {
 
 function main() {
 
-    console.log("HERE");
     initBoard();
     solve(0,0);
 }
@@ -63,15 +67,15 @@ function main() {
 function solve (row: number, col: number) {
     if (row > 8){
         // finished computing the board
+        console.log("Solution Reached");
         console.log(model);
     }
 
-    console.log("REACHING HERE");
     if (model[row][col] != 0)
         nextElem (row, col);
     else{
         for (let i: number = 1; i <= 9; i++){
-            if(!checkRow(row, i) && !checkCol(col, i) && !checkBox(row, col, i)){
+            if(checkRow(row, i) && checkCol(col, i) && checkBox(row, col, i)){
                 model[row][col] = i;
                 nextElem (row, col);
             }
@@ -100,33 +104,32 @@ function nextElem (row: number, col: number){
 function checkRow (row: number, value: number): number{
     for (let i: number = 0; i < modelLength; i++){
         if (value == model[row][i])
-            return 1;
+            return 0;
     }
-    return 0;
+    return 1;
 }
 
 // checks if the element is present in the column
 function checkCol (col: number, value: number): number{
     for (let i: number = 0; i < modelLength; i++){
         if (value == model[i][col])
-            return 1;
+            return 0;
     }
-    return 0;
+    return 1;
 }
 
 // checks if the element is present in the box
 function checkBox (row: number, col: number, value: number): number{
-    row = (row % 3) * 3;
-    col = (col % 3) * 3;
+    row = (row / 3) * 3;
+    col = (col / 3) * 3;
 
     for (let i: number = 0; i < 3; i++){
         for (let j: number = 0; j < 3; j++){
             if (model[row + i][col + j] == value)
-                return 1;
+                return 0;
         }
     }
-
-    return 0;
+    return 1;
 }
 
 
