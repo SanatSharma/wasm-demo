92b9981cfdb2136601da2e9283ccecfcea4875e7
Ignacio Ramirez Piriz <t-igrami@microsoft.com>
2017-07-18 18:05:08 -0700

    Iterative solution

--- lib/sudoku/solver.ts
+++ lib/sudoku/solver.ts
@@ -1,22 +1,21 @@
-type int = number;
 
-let modelLength: int = 9;
-let model: int[][] = new Array(9);
 
-export function initBoard(): void {
+var modelLength: int = 9;
+var model: int[][] = new Array(9);
+var preSetNumbers: int[][] = new Array(9);
+
+function initBoard(): void {
     let i: int = 0;
     let j: int = 0;
-
-    for (i = 0; i < modelLength; i++){
+    for (i = 0; i < modelLength; i++) {
         model[i] = new Array(9);
-        for (j = 0; j < modelLength; j++){
+        for (j = 0; j < modelLength; j++) {
             model[i][j] = 0;
         }
-    }
+    }  
 
-    
-    // Create the initial board
-    model[0][0] = 9 ;
+    //console.log("init arr " + model);
+     model[0][0] = 9 ;
     model[0][3] = 2 ;
     model[0][6] = 7 ;
     model[0][7] = 5 ;
@@ -54,76 +53,216 @@ export function initBoard(): void {
     model[8][1] = 8 ;
     model[8][2] = 2 ;
     model[8][4] = 4 ;
-    //model[8][8] = 6 ;
-}
+ //   model[8][8] = 6 ;
 
-export function main(): void {
-    solve(0,0);
-}
-
-function solve (row: int, col: int): void {
-    if (row > 8){
-        // finished computing the board
-    }
-
-    else if (model[row][col] != 0)
-        nextElem (row, col);
-    else{
-        for (let i: int = 1; i <= 9; i++){
-            if(checkRow(row, i) && checkCol(col, i) && checkBox(row, col, i)){
-                model[row][col] = i;
-                nextElem (row, col);
+    // Create the initial board
+//     model[0][0] = randomArr[0];
+//     model[0][3] = randomArr[1];
+//     model[0][6] = randomArr[2];
+//     model[0][7] = randomArr[3];
+//     model[1][0] = randomArr[4];
+//     model[1][4] = randomArr[5];
+//     model[1][7] = randomArr[6];
+//     model[2][1] = randomArr[7];
+//     model[2][3] = randomArr[8];
+//     //model[2][7] = randomArr[9];
+//     model[2][6] = randomArr[10];
+//     //model[3][0] = randomArr[11];
+//     model[3][2] = randomArr[12];
+//     //model[4][1] = randomArr[13];
+//     model[4][3] = randomArr[14];
+//     model[4][5] = randomArr[15];
+//     model[4][7] = randomArr[16];
+//     model[5][6] = randomArr[17];
+//     model[5][8] = randomArr[18];
+//     model[6][1] = randomArr[19];
+//     model[6][5] = randomArr[20];
+//     model[6][7] = randomArr[21];
+//     model[7][1] = randomArr[22];
+//     model[7][4] = randomArr[23];
+//     model[7][8] = randomArr[24];
+//     model[8][1] = randomArr[25];
+//     model[8][2] = randomArr[26];
+//     model[8][4] = randomArr[27];
+//     // model[8][8] = randomArr[28];
+//    // console.log("init " +model);
+   for (i = 0; i < 9; i++) {
+        preSetNumbers[i] = new Array(9);
+        for (j = 0; j < 9; j++) {
+            if(model[i][j] == 0) {
+                preSetNumbers[i][j] = 0;
+            }
+            else {
+                preSetNumbers[i][j] = 1;
             }
         }
-
-        model[row][col] = 0;
-
-    }
-
-}
-
-function nextElem (row: int, col: int): void {
-    if (col < 8){
-        // can go to next element in column
-        solve (row, col + 1);
-    }
-    else{
-        // reached the end of the row. Move on to the next row
-        solve (row + 1, 0);
     }
 }
 
-// return int instead of boolean to work with AssemblyScript
+function main(): void {
+    //console.log(model);
+    initBoard();
+    //solve(0,0);
+    let bool: int = 0;
+    let firstTime: int = 0;
+    let k: int = 0;
+    let firstValueIndex: int = 0;
+    let goingBackLoop: int = 0;
+    let goingForward: int = 1;
+    let indexLastGBLoop: int = 10;
+    for(let i: int = 0; i < 9; i++) {
+        //console.log("new row");
+        for(let j: int = 0; j < 9; j++) {
+            if(i == 3 && j == 1) {
+               // console.log(j);
+            }
+            if(bool == 0 && model[i][j] == 0) {
+                firstTime = 1;
+            }
 
-// checks if the element is present in the row
-function checkRow (row: int, value: int): int{
-    for (let i: int = 0; i < modelLength; i++){
+            if(firstTime == 1) {
+                bool = 1;
+                if(goingBackLoop == 1 && goingForward == 0) {
+                    k = model[i][j];
+                    if(k >= firstValueIndex) {
+                       if(k == firstValueIndex) {
+                           k++;
+                       } 
+                       while(k < 10 && bool == 1) {
+                          if(checkRow(i, k) == 1 && checkCol(j, k) == 1) {
+                            model[i][j] = k;
+                            bool = 0;
+                            firstTime = 0;
+                            goingForward = 1;
+                          }
+                          k++; 
+                       }
+                    }
+                }
+                else {
+                    k = 1;
+                    while(k < 10 && bool == 1) {
+                        if(checkRow(i, k) == 1 && checkCol(j, k) == 1) {
+                            model[i][j] = k;
+                            bool = 0;
+                            firstTime = 0;
+                            goingBackLoop = 0;
+                            goingForward = 1;
+                        }
+                        k++;
+                    }
+                } 
+            } 
+            if(bool == 1) {
+                if(j == 0) {
+                    if(preSetNumbers[i][j] == 0){
+                        model[i][j] = 0;
+                    }
+                    
+                    if(goingBackLoop == 0 || indexLastGBLoop == j) {
+                        firstValueIndex = model[i - 1][8];
+                        indexLastGBLoop = 8;
+                    }
+                    i = i - 1;
+                    j = 7;
+                    goingBackLoop = 1;
+                    goingForward = 0;
+                }
+                else {
+                    if(preSetNumbers[i][j] == 0) {
+                        model[i][j] = 0;
+                    }
+                    if(goingBackLoop == 0 || indexLastGBLoop == j) {
+                        firstValueIndex = model[i][j - 1];
+                        indexLastGBLoop = j - 1;
+                    }
+                    j = j - 2;
+                    goingBackLoop = 1;
+                    goingForward = 0;
+                }
+            }
+
+        }
+    }
+    // for(let h = 0; h < 9; h++) {
+    //     console.log(model[h]);
+    // }
+    //console.log(model);
+
+}
+
+//RECURSIVE SOLUTION
+
+//function solve(row, col) {
+//     if (row > 8) {
+//         // finished computing the board
+//         //console.log("solution arr " + model);
+//         //const after = performance.now()
+//         //time = (after - before);
+//         //console.log(time);
+//          console.log(model);
+//         // throw new Error("solution");
+//         throw new Error("solution");
+//     }
+//     else if (model[row][col] != 0) {
+//          nextElem(row, col);
+//     }
+//     else {
+//         for (var i = 1; i <= 9; i++) {
+//             if (checkRow(row, i) && checkCol(col, i) && checkBox(row, col, i)) {
+//                 model[row][col] = i;
+//                  nextElem(row, col);
+        
+//             }
+//         }
+//         model[row][col] = 0;
+//     }
+// }
+// function nextElem(row, col) {
+//     if (col < 8) {
+//         // can go to next element in column
+//         return solve(row, col + 1);
+//     }
+//     else {
+//         // reached the end of the row. Move on to the next row
+//         return solve(row + 1, 0);
+//     }
+// }
+// // return int instead of boolean to work with AssemblyScript
+// // checks if the element is present in the row
+
+function checkRow(row: int, value: int): int {
+    for (var i: int = 0; i < modelLength; i++) {
         if (value == model[row][i])
             return 0;
     }
     return 1;
 }
-
-// checks if the element is present in the column
-function checkCol (col: int, value: int): int{
-    for (let i: int = 0; i < modelLength; i++){
+// // checks if the element is present in the column
+function checkCol(col: int, value: int): int {
+    for (var i: int = 0; i < modelLength; i++) {
         if (value == model[i][col])
             return 0;
     }
     return 1;
 }
-
 // checks if the element is present in the box
-function checkBox (row: int, col: int, value: int): int{
-    // Need to floor to get integer value
-    row = ((row / 3) | 0) * 3;
-    col = ((col / 3) | 0) * 3;
+// function checkBox(row, col, value) {
+//     // Need to floor to get integer value
+//     row = ((row / 3) | 0) * 3;
+//     col = ((col / 3) | 0) * 3;
+//     for (var i = 0; i < 3; i++) {
+//         for (var j = 0; j < 3; j++) {
+//             if (model[row + i][col + j] == value)
+//                 return 0;
+//         }
+//     }
+//     return 1;
+// }
 
-    for (let i: int = 0; i < 3; i++){
-        for (let j: int = 0; j < 3; j++){
-            if (model[row + i][col + j] == value)
-                return 0;
-        }
-    }
-    return 1;
-}
+// function getJSModel() {
+//     return model;
+// }
+// function getTime() {
+//     return time;
+// }
