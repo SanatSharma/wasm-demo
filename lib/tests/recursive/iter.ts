ab494d05ae462f1f4fc2f696c141446e8bc6a603
Sanat Sharma <sanatcolumban@gmail.com>
2017-08-02 14:15:28 -0700

    updated demo

--- lib/tests/recursive/iter.ts
+++ lib/tests/recursive/iter.ts
@@ -1,8 +1,9 @@
-export function fib(): number {
-  let n: number = 1000;
-  let i: number, t: number, a: number = 0, b: number = 1;
+export function fib(n: double): double {
+  let i: int, t: double , a: double = 0, b: double = 1;
   for (i = 0; i < n; i++) {
-    t = a + b; a = b; b = t;
+    t = a + b; 
+    a = b; 
+    b = t;
   }
   return b;
 }
